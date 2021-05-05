dcfa9104110f0d639548a54626a911b3d1d929c2
Tsungyu Yu <ytyubox@gmail.com>
2021-05-05 23:13:54 +0800

    Spec thirty love
    Fix: miss delete Fifteen love

--- Tests/TennisTests/Tennis.swift
+++ Tests/TennisTests/Tennis.swift
@@ -4,6 +4,10 @@ class Tennis {
         if firstPlayerScoreTime == 1 {
             return "fifteen love"
         }
+        if firstPlayerScoreTime == 2 {
+            return "thirty love"
+        }
+
         return "love all"
     }
     func firstPlayerScore() {
--- Tests/TennisTests/TennisTests.swift
+++ Tests/TennisTests/TennisTests.swift
@@ -1,23 +1,28 @@
 import Nimble
 import Quick
 
-
 final class TennisTests: QuickSpec {
-    
     override func spec() {
         describe("Score") {
-            
-            let tennis = Tennis()
+            var tennis = Tennis()
+            beforeEach {
+                tennis = Tennis()
+            }
             func scrollShouldBe(_ score: String, file: StaticString = #filePath, line: UInt = #line) {
                 expect(file: file, line: line, tennis.score()).to(equal(score))
             }
-            
             it("Should be love all") {
                 scrollShouldBe("love all")
             }
             it("Should be fifteen love") {
                 tennis.firstPlayerScore()
                 scrollShouldBe("fifteen love")
+            }
+
+            it("Should be thirty love") {
+                tennis.firstPlayerScore()
+                tennis.firstPlayerScore()
+                scrollShouldBe("thirty love")
             }
         }
     }
